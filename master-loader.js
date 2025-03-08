836a28c11beea9a6aee1b301cab4bf1b475afda5
secretren <renupio@proton.me>
2025-03-07 21:19:52 -0600

    Update master-loader.js

--- master-loader.js
+++ master-loader.js
@@ -3,7 +3,7 @@ var scripts = document.getElementsByTagName("script"),
     scriptUrl = scripts[scripts.length - 1].src,
     root = scriptUrl.split("master-loader.js")[0],
     loaders = {
-        unity: "unity.js"
+        unity: "/unity.js"
     };
 if (0 <= window.location.href.indexOf("pokiForceLocalLoader") && (loaders.unity = "./unity.js", root = "/loaders"), !window.config) throw Error("window.config not found");
 var loader = loaders[window.config.loader];
@@ -12,10 +12,10 @@ if (!window.config.unityWebglLoaderUrl) {
     var versionSplit = window.config.unityVersion ? window.config.unityVersion.split(".") : [],
         year = versionSplit[0],
         minor = versionSplit[1];
-    window.config.unityWebglLoaderUrl = "./UnityLoader.2019.2.js";
+    window.config.unityWebglLoaderUrl = "/UnityLoader.2019.2.js";
 }
 var sdkScript = document.createElement("script");
-sdkScript.src = "./poki-sdk.js", sdkScript.onload = function() {
+sdkScript.src = "poki-sdk.js", sdkScript.onload = function() {
     var i = document.createElement("script");
     i.src = root + loader, document.body.appendChild(i)
 }, document.body.appendChild(sdkScript);
